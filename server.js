d02fa82795e474510d2a193e20a1f497bd27c3ff
quanfannaruto4-lab <quanfannaruto4@gmail.com>
2026-03-14 23:31:11 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,38 +1,89 @@
 const express = require("express");
+const fetch = require("node-fetch");
 
 const app = express();
 
+let visitors = [];
+
 app.get("/", async (req, res) => {
 
-  const ip = req.headers["x-forwarded-for"]?.split(",")[0] || "8.8.8.8";
+  const ip =
+    req.headers["x-forwarded-for"]?.split(",")[0] ||
+    req.socket.remoteAddress ||
+    "8.8.8.8";
 
   try {
 
-    const response = await fetch(`http://ip-api.com/json/${ip}`);
+    const response = await fetch(`https://ipapi.co/${ip}/json/`);
     const data = await response.json();
+
+    const visitor = {
+      ip: ip,
+      city: data.city,
+      country: data.country_name,
+      lat: data.latitude,
+      lon: data.longitude,
+      time: new Date().toLocaleString()
+    };
+
+    visitors.push(visitor);
 
     res.send(`
       <h2>IP Info</h2>
-      IP: ${ip}<br>
-      City: ${data.city}<br>
-      Country: ${data.country}<br>
-      Latitude: ${data.lat}<br>
-      Longitude: ${data.lon}<br><br>
+      IP: ${visitor.ip}<br>
+      City: ${visitor.city}<br>
+      Country: ${visitor.country}<br>
+      Latitude: ${visitor.lat}<br>
+      Longitude: ${visitor.lon}<br><br>
 
-      <a href="https://www.google.com/maps?q=${data.lat},${data.lon}" target="_blank">
+      <a href="https://www.google.com/maps?q=${visitor.lat},${visitor.lon}" target="_blank">
       Xem vị trí trên Google Maps
       </a>
+
+      <br><br>
+      <a href="/dashboard">Xem Dashboard</a>
     `);
 
-  } catch (err) {
-
-    console.log(err);
+  } catch (e) {
     res.send("Error getting IP info");
-
   }
 
 });
 
-app.listen(process.env.PORT || 3000, () => {
+app.get("/dashboard", (req, res) => {
+
+  let table = visitors
+    .map(v => `
+      <tr>
+        <td>${v.ip}</td>
+        <td>${v.city}</td>
+        <td>${v.country}</td>
+        <td>${v.lat}, ${v.lon}</td>
+        <td>${v.time}</td>
+      </tr>
+    `)
+    .join("");
+
+  res.send(`
+    <h2>Visitor Dashboard</h2>
+
+    <table border="1" cellpadding="8">
+      <tr>
+        <th>IP</th>
+        <th>City</th>
+        <th>Country</th>
+        <th>Coordinates</th>
+        <th>Time</th>
+      </tr>
+      ${table}
+    </table>
+
+    <br>
+    <a href="/">Back</a>
+  `);
+
+});
+
+app.listen(3000, () => {
   console.log("Server running");
 });
