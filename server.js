c9004330c17ed495920868b1d20316ae88e2f4dc
quanfannaruto4-lab <quanfannaruto4@gmail.com>
2026-03-14 22:25:52 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,42 @@
 const express = require("express");
+const https = require("https");
 const fetch = require("node-fetch");
 
 const app = express();
 
+app.get("/", (req, res) => {
+  const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
 app.get("/", async (req, res) => {
 
+  https.get(`https://ipapi.co/${ip}/json/`, (resp) => {
+    let data = "";
   const ip = req.headers["x-forwarded-for"]?.split(",")[0] || req.socket.remoteAddress;
 
+    resp.on("data", chunk => {
+      data += chunk;
+    });
   try {
 
+    resp.on("end", () => {
+      const info = JSON.parse(data);
     const response = await fetch(`https://ipapi.co/${ip}/json/`);
     const data = await response.json();
 
+      const city = info.city;
+      const country = info.country_name;
     const city = data.city;
     const country = data.country_name;
     const lat = data.latitude;
     const lon = data.longitude;
 
+      console.log("IP:", ip);
+      console.log("City:", city);
     console.log("IP:", ip);
     console.log("City:", city);
     console.log("Coordinates:", lat, lon);
 
+      res.send(`IP: ${ip} <br> Thành phố: ${city} <br> Quốc gia: ${country}`);
+    });
     res.send(`
       <h2>Visitor Info</h2>
       IP: ${ip} <br>
@@ -38,8 +54,12 @@ app.get("/", async (req, res) => {
     res.send("Không lấy được vị trí IP");
   }
 
+  }).on("error", () => {
+    res.send("Không tra được IP");
+  });
 });
 
+app.listen(3000);
 app.listen(3000, () => {
   console.log("Server running...");
 });
