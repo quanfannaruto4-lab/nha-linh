6697879ef248c52013fc4a582304847e6e2b07c0
quanfannaruto4-lab <quanfannaruto4@gmail.com>
2026-03-14 21:48:32 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+  console.log("Visitor IP:", ip);
+  res.send("IP của bạn: " + ip);
+});
+
+app.listen(3000);
